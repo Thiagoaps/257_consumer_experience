27cf3a12b7463034226f562742b88452a8049db8
Thiago Antonio <thiagoapsx@gmail.com>
2024-06-09 16:32:29 -0300

    add Locale

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,14 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 		
 		list.add(new Product("Tv", 900.00));
